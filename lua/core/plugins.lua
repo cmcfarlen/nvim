046d41fe734ad3d0e0ada925e909d4f12bf2a2de
Chris McFarlen <chris@mcfarlen.us>
2023-08-11 16:40:07 -0500

    remove lsp-zero and replace with base plugins. inlay hints work!

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -30,32 +30,17 @@ return require('packer').startup(function(use)
 
   use 'mfussenegger/nvim-dap'
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd.MasonUpdate)
-			end,
-			},
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'L3MON4D3/LuaSnip'
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},         -- Required
-			{'hrsh7th/cmp-nvim-lsp'},     -- Required
-			{'hrsh7th/cmp-buffer'},       -- Optional
-			{'hrsh7th/cmp-path'},         -- Optional
-			{'saadparwaiz1/cmp_luasnip'}, -- Optional
-			{'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},             -- Required
-			{'rafamadriz/friendly-snippets'}, -- Optional
-		},
-	}
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
 
   use 'ilyachur/cmake4vim'
 
