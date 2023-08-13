eb988edafc5677ae70edd4a5dde806a468fdf608
Chris McFarlen <chris@mcfarlen.us>
2023-08-13 14:43:47 -0500

    Update cmp config. Fix LspInlayHint color in dracula theme. Fix the issue with quick-scope ignoring the hightlight on key setting

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,3 +1,8 @@
+
+
+-- quick-scope only reads the value of this at load time, so needs to go up front here
+vim.g.qs_highlight_on_keys = {'f', 't', 'F', 'T'}
+
 require("core.plugins")
 require("core.remap")
 require("core.set")
