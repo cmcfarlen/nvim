36b12f900c5b369c01ace50914fdad4b56a5c881
Chris McFarlen <cmcfarlen@apple.com>
2025-11-18 05:31:51 -0600

    cleanup

--- lua/plugins/ctest.lua
+++ lua/plugins/ctest.lua
@@ -7,7 +7,7 @@ return {
 		"orjangj/neotest-ctest",
 		"Shatur/neovim-tasks",
 		"rosstang/lunajson.nvim",
-		{ "rosstang/neotest-catch2", branch = "use-catch2-with-json" },
+		--{ "rosstang/neotest-catch2", branch = "use-catch2-with-json" },
 		{
 			"neotest-swift",
 			url = "git@github.pie.apple.com:jerryjrchen/neotest-swift.git",
@@ -26,14 +26,39 @@ return {
 				end,
 			},
 		}, neotest_ns)
+		local lib = require("neotest.lib")
 
 		require("neotest").setup({
 			--log_level = vim.log.levels.DEBUG,
 			adapters = {
 				require("neotest-ctest").setup({}),
 				-- Load with default config
-				require("neotest-catch2")(),
+				--require("neotest-catch2")(),
 				require("neotest-swift"),
+			},
+			projects = {
+				["~/projects/oss/trafficserver"] = {
+					discovery = {
+						enabled = true,
+						concurrent = 0,
+						filter_dir = function(name, rel_path, root)
+							return name ~= "build"
+						end,
+					},
+					adapters = {
+						require("neotest-ctest").setup({
+							frameworks = { "catch2" },
+							is_test_file = function(file_path)
+								local elems = vim.split(file_path, lib.files.sep, { plain = true })
+								local name, extension = unpack(vim.split(elems[#elems], ".", { plain = true }))
+								local supported_extensions = { "cpp", "cc", "cxx" }
+								return vim.tbl_contains(supported_extensions, extension)
+										and (vim.startswith(name, "test_") or vim.endswith(name, "_test"))
+									or false
+							end,
+						}),
+					},
+				},
 			},
 		})
 	end,
